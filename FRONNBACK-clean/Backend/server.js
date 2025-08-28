2d1d8e2a67ff733080f88c4abc22c4ddec1e105a
Narahari-11 <bhutanalnarahari@gmail.com>
2025-08-28 22:49:05 +0530

    Update server.js

--- FRONNBACK-clean/Backend/server.js
+++ FRONNBACK-clean/Backend/server.js
@@ -14,17 +14,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files
+app.use(express.static(path.join(__dirname, '../Frontend/public')));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// Route to serve the registration form
+// Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+    res.sendFile(path.join(__dirname, '../Frontend/public', 'index.html'));
+});
+
+app.get('/map', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/public', 'map.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/public', 'register.html'));
 });
 
 // Route to handle form submission
